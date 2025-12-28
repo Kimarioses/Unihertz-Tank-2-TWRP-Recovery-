c9003de576b54f3880f87cc2d3c525086be775ca
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-12-28 01:05:07 +0100

    Update twrp_TANK2.mk

--- twrp_TANK2.mk
+++ twrp_TANK2.mk
@@ -6,14 +6,27 @@
 # Release
 PRODUCT_RELEASE_NAME := TANK2
 
-# Inherit from base
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
-# Inherit TWRP common config (TWRP 14)
+# Configure twrp config common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Device path
+# Device specific configs
 $(call inherit-product, device/oblue/TANK2/device.mk)
+
+# Configure launch_with_vendor_ramdisk.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+# Configure compression
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
+# Configure virtual_ab_ota.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Identifiers
 PRODUCT_DEVICE := TANK2
